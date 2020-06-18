12ec22ec9f3001c10a3163c325da1a1b0f883a0b
Aleksandr Ovchinin <alex.ovchinin@gmail.com>
2020-06-18 23:49:48 +0500

    Initial commit

---
+++ src/main/java/com/fm/virtue/VirtueApplication.java
@@ -0,0 +1,13 @@
+package com.fm.virtue;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VirtueApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VirtueApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fm/virtue/VirtueApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fm.virtue;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VirtueApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
