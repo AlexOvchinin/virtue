c377240f37790bd9ab6cd60e31a45b3e93d43385
Aleksandr Ovchinin <alex.ovchinin@gmail.com>
2020-06-19 20:07:21 +0500

    Run configuration + application config

---
+++ src/main/java/com/fm/virtue/TestConfig.java
@@ -0,0 +1,30 @@
+package com.fm.virtue;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableConfigurationProperties
+@ConfigurationProperties(prefix = "test.config")
+public class TestConfig {
+
+    private String property1;
+    private String property2;
+
+    public String getProperty1() {
+        return property1;
+    }
+
+    public void setProperty1(String property1) {
+        this.property1 = property1;
+    }
+
+    public String getProperty2() {
+        return property2;
+    }
+
+    public void setProperty2(String property2) {
+        this.property2 = property2;
+    }
+}
--- src/main/java/com/fm/virtue/VirtueApplication.java
+++ src/main/java/com/fm/virtue/VirtueApplication.java
@@ -1,13 +1,23 @@
 package com.fm.virtue;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class VirtueApplication {
+public class VirtueApplication implements CommandLineRunner {
+
+	@Autowired
+	private TestConfig testConfig;
 
 	public static void main(String[] args) {
 		SpringApplication.run(VirtueApplication.class, args);
+	}
+
+	public void run(String... args) {
+		System.out.println("Property1: " + testConfig.getProperty1());
+		System.out.println("Property2: " + testConfig.getProperty2());
 	}
 
 }
